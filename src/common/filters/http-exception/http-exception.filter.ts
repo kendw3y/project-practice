a4673164f3190bb792869dc23e2e2b4decdefab3
karel <karelmartinez003@gmail.com>
2026-09-07 14:26:00 -0400

    actualizando exceptionfilter

--- src/common/filters/http-exception/http-exception.filter.ts
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -1,9 +1,30 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse()
+
+    const status = exception instanceof HttpException
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR
+    
+    const exceptionResponse = exception instanceof HttpException
+    ? exception.getResponse()
+    : 'Error interno del servidor'
+
+    const message = typeof exceptionResponse === 'string'
+    ?exceptionResponse
+    :(exceptionResponse as any).message || 'Error desconocido'
+
+    const errorResponse={
+      success:false,
+      statusCode:status,
+      message,
+      timestamp: new Date().toISOString()
+    }
+
+    response.status(status).json(errorResponse)
   }
 }
